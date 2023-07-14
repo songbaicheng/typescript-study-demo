625501ad0cb4ef4744e4c1dbe575d5fbf8d085b8
songbaicheng <2524218694@qq.com>
2023-07-14 14:17:56 +0800

    add base type code

--- base-type/index.js
+++ base-type/index.js
@@ -1,3 +1,25 @@
 "use strict";
+//  字符串类型
 let str = 'songbaicheng';
 console.log(str);
+// 数字类型
+let notANumber = NaN; // Nan
+let num = 123; // 普通数字
+let infinityNumber = Infinity; // 无穷大
+let decimal = 6; // 十进制
+let hex = 0xf00d; // 十六进制
+let binary = 0b1010; // 二进制
+let octal = 0o744; // 八进制
+// 布尔类型
+let booleand1 = true; //可以直接使用布尔值
+let booleand2 = Boolean(1); //也可以通过函数返回布尔值
+console.log(booleand1);
+console.log(booleand2);
+// 空值类型
+function voidFn() {
+    console.log('test void');
+}
+voidFn();
+// Null和undefined类型
+let u = undefined;
+let n = null;
--- base-type/index.ts
+++ base-type/index.ts
@@ -1,3 +1,31 @@
-let str:string = 'songbaicheng'
+//  字符串类型
+let str: string = 'songbaicheng'
 
 console.log(str)
+
+// 数字类型
+let notANumber: number = NaN; // Nan
+let num: number = 123; // 普通数字
+let infinityNumber: number = Infinity; // 无穷大
+let decimal: number = 6; // 十进制
+let hex: number = 0xf00d; // 十六进制
+let binary: number = 0b1010; // 二进制
+let octal: number = 0o744; // 八进制
+
+// 布尔类型
+let booleand1: boolean = true //可以直接使用布尔值
+let booleand2: boolean = Boolean(1) //也可以通过函数返回布尔值
+
+console.log(booleand1)
+console.log(booleand2)
+
+// 空值类型
+function voidFn(): void {
+    console.log('test void')
+}
+
+voidFn()
+
+// Null和undefined类型
+let u: undefined = undefined;
+let n: null = null;
